be0af6b444a39a5cea0eca3cd59285403a658349
KirilTan <kiriltanev@proton.me>
2023-10-05 20:33:54 +0300

    Added solution for lab exercise 03

---
+++ 02_data_types_and_variables/lab/03_pounds_to_dollars.py
@@ -0,0 +1,11 @@
+# Input
+GBP_value = float(input())
+
+# Calculations
+covertion_rate = 1.31
+USD_value = GBP_value * covertion_rate
+
+text_for_print = f"{USD_value:.3f}"
+
+# Output
+print(text_for_print)
