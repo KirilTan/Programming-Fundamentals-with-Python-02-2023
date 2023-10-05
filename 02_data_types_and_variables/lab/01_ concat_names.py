242729436d865a4dcef6ded3a9f2c2692acdfce4
KirilTan <kiriltanev@proton.me>
2023-10-05 19:58:26 +0300

    Updated solution to lab exercise 01

--- 02_data_types_and_variables/lab/01_ concat_names.py
+++ 02_data_types_and_variables/lab/01_ concat_names.py
@@ -4,7 +4,7 @@ last_name = str(input())
 delimiter = str(input())
 
 # Calculation
-print_text = first_name + delimiter + last_name
+print_text = f"{first_name}{delimiter}{last_name}"
 
 # Print
 print(print_text)
