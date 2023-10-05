e45f6888edcfa4b473d44933fe5def67f3b857f6
KirilTan <kiriltanev@proton.me>
2023-10-05 19:55:53 +0300

    Updated file types and added solution to lab exercise 01

---
+++ 02_data_types_and_variables/lab/01_ concat_names.py
@@ -0,0 +1,10 @@
+# Input
+first_name = str(input())
+last_name = str(input())
+delimiter = str(input())
+
+# Calculation
+print_text = first_name + delimiter + last_name
+
+# Print
+print(print_text)
