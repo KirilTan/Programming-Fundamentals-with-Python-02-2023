2004f511465bbeaa764b96c7e31e464cf705aa35
KirilTan <kiriltanev@proton.me>
2023-10-05 22:50:15 +0300

    Updated solution for lab exercise 06.1

--- 02_data_types_and_variables/lab/06.1_next_happy_year.py
+++ 02_data_types_and_variables/lab/06.1_next_happy_year.py
@@ -3,14 +3,18 @@ year = int(input())
 year_is_special = False
 
 # Calculations
+
 while not year_is_special:
+
     year += 1
     year_as_string = str(year)
+
+
     year_is_special = True
     for digit in year_as_string:
         if year_as_string.count(digit) > 1:
             year_is_special = False
             break
-    
+
 # Output
 print(year)
