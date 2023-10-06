6a7d5a022b6f6955c0f889fa2287fe58a000ee4b
KirilTan <kiriltanev@proton.me>
2023-10-06 21:59:39 +0300

    Solution to exercise 08 added

---
+++ 02_data_types_and_variables/exercise/08_party_profit.py
@@ -0,0 +1,27 @@
+from math import floor
+
+# Inputs
+group_size = int(input())
+days = int(input())
+
+# Calculations
+coins = 0
+for current_day in range(1, days + 1):
+
+    if current_day % 10 == 0:
+        group_size -= 2
+    if current_day % 15 == 0:
+        group_size += 5
+
+    coins += 50              # Daily coin gain
+    coins -= group_size * 2  # Daily rations
+
+    if current_day % 3 == 0:
+        coins -= group_size * 3     # Water rations
+    if current_day % 5 == 0:
+        coins += group_size * 20    # Boss slain reward
+        if current_day % 3 == 0:
+            coins -= group_size * 2
+
+# Output
+print(f"{group_size} companions received {floor(coins / group_size)} coins each.")
