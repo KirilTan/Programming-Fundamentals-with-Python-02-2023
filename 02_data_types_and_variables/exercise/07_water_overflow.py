8ed60fb263212807fda3b3aabcade1dd4c5ca94f
KirilTan <kiriltanev@proton.me>
2023-10-06 21:39:54 +0300

    Solution to exercise 07 added

---
+++ 02_data_types_and_variables/exercise/07_water_overflow.py
@@ -0,0 +1,16 @@
+# Inputs and constants
+number_of_inputs = int(input())
+capacity = 255  # In liters
+
+# Calculations
+capacity_left = capacity
+water_poured = 0
+for line in range(number_of_inputs):
+    current_input = int(input())
+    if capacity_left - current_input >= 0:
+        capacity_left -= current_input
+        water_poured += current_input
+    else:
+        print("Insufficient capacity!")
+
+print(water_poured)
