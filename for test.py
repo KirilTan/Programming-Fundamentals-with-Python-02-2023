d7ba4688a918839a7f851297dd488a90507e99d9
KirilTan <kiriltanev@proton.me>
2023-10-10 21:26:09 +0300

    Added folder to hold 03_exercises and 03_lab

--- for test.py
+++ for test.py
@@ -1,16 +1,6 @@
-number_of_inputs = int(input())
-magic_word = input()
+txt = "welcome to the jungle"
 
-all_strings = []
-for _ in range(number_of_inputs):
-    current_string = input()
-    all_strings.append(current_string)
+x = txt.split()
 
-print(all_strings)
-
-filtered_strings = []
-for current_string in all_strings:
-    if current_string in all_strings:
-        filtered_strings.append(current_string)
-
-print(filtered_strings)
+print(x)
+print(type(x))
