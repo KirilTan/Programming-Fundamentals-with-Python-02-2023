b7059f1a7b2d4b3526f58e83883c41f01d2a4e68
KirilTan <kiriltanev@proton.me>
2023-10-15 15:32:28 +0300

    Added solution to more_exercises_02

---
+++ 03_lists_basics/more_exercises/02_messaging.py
@@ -0,0 +1,16 @@
+input_list = input().split()
+input_str = input()
+
+output_list = []
+for current_number in input_list:
+
+    current_number_sum = 0
+    for i in current_number:
+        current_number_sum += int(i)
+
+    current_number_sum %= len(input_str)
+
+    output_list.append(input_str[current_number_sum])
+    input_str = input_str.replace(input_str[current_number_sum], '', 1)
+
+print(*output_list, sep="")
