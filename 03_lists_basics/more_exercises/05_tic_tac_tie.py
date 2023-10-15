5827bd7df0b25f82798f983421a67bfdbbd9bf2a
KirilTan <kiriltanev@proton.me>
2023-10-15 16:55:43 +0300

    Added solution to more_exercises_05

---
+++ 03_lists_basics/more_exercises/05_tic_tac_tie.py
@@ -0,0 +1,31 @@
+def check_winner(board):
+    # Check rows and columns
+    for i in range(3):
+        if all(board[i][j] == 1 for j in range(3)):
+            return "First player won"
+        elif all(board[j][i] == 1 for j in range(3)):
+            return "First player won"
+        elif all(board[i][j] == 2 for j in range(3)):
+            return "Second player won"
+        elif all(board[j][i] == 2 for j in range(3)):
+            return "Second player won"
+
+    # Check diagonals
+    if all(board[i][i] == 1 for i in range(3)):
+        return "First player won"
+    elif all(board[i][2 - i] == 1 for i in range(3)):
+        return "First player won"
+    elif all(board[i][i] == 2 for i in range(3)):
+        return "Second player won"
+    elif all(board[i][2 - i] == 2 for i in range(3)):
+        return "Second player won"
+
+    return "Draw!"
+
+
+# Input
+board = [list(map(int, input().split())) for _ in range(3)]
+
+# Output
+result = check_winner(board)
+print(result)
