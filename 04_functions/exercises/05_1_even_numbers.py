eae70caab5841239028c23e22b68cb7d00dcd520
KirilTan <kiriltanev@proton.me>
2023-10-17 21:34:24 +0300

    Updated solution to 04_exercise_05_1

--- 04_functions/exercises/05_1_even_numbers.py
+++ 04_functions/exercises/05_1_even_numbers.py
@@ -1,6 +1,4 @@
 my_list = [int(s) for s in input().split()]
 
-is_even = lambda x: x % 2 == 0
-
-result = list(filter(is_even, my_list))
+result = list(filter(lambda x: x % 2 == 0, my_list))
 print(result)
