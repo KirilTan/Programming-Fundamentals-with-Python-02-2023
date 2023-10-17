099596888eb7b8e796776bf5b0fce4c30d97cc67
KirilTan <kiriltanev@proton.me>
2023-10-17 21:27:29 +0300

    Updated solution to 04_exercise_05_2

--- 04_functions/exercises/05_2_even_numbers.py
+++ 04_functions/exercises/05_2_even_numbers.py
@@ -1,8 +1,5 @@
 def is_even(number: int) -> bool:
-    if number % 2 == 0:
-        return True
-    else:
-        return False
+    return number % 2 == 0
 
 
 my_list = [int(s) for s in input().split()]
