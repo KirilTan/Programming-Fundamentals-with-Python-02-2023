9f783774ffb15123be1449f109ceebfb0d70c3fd
KirilTan <kiriltanev@proton.me>
2023-10-20 12:44:11 +0300

    Updated solution to 04_more_exercises_04

--- 04_functions/more_exercises/04_tribonacci_sequeence.py
+++ 04_functions/more_exercises/04_tribonacci_sequeence.py
@@ -1,12 +1,14 @@
-def tribonacci(n):
+def tribonacci_sequence(n: int) -> list:
     sequence = [1, 1, 2]
 
     for i in range(3, n):
         next_term = sequence[i - 1] + sequence[i - 2] + sequence[i - 3]
         sequence.append(next_term)
 
-    print(*sequence[:n])
+    return sequence
+
 
 # Take user input for the number of terms
-n = int(input("Enter the number of Tribonacci terms: "))
-tribonacci(n)
+num = int(input())
+output_text = tribonacci_sequence(num)
+print(*output_text[:num])
